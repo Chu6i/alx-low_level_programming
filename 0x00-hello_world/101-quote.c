67ba9d774e2f7d64b2887cf4736bb2178c1966c8
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-10 13:16:50 -0800

    Add a program that prints a line of word, then a standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -Program prints a line of code, then a standard error
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
