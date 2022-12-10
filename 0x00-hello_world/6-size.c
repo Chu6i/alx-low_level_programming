4ebf347bc3cd3619252a0a9d2e17f63dd8ba0d5a
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-10 10:23:43 -0800

    Fixed the scripting for 32 and 64 bit memory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ float floatType;
 long a;
 long long b;
 char charType;
-	printf("size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("size of a long int: %zu byte(s)\n", sizeof(a));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(b));
-	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
