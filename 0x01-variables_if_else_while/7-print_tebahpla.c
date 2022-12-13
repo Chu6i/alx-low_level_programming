4182f27852b8ac0c790d73f50cc4e35e52730b61
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-12 18:13:27 -0800

    Fix the descriptive function for main

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* Main -Entry point
+* main -Entry point
 *
 * Return: 0
 */
@@ -14,5 +14,5 @@ for (; reverse >= 97; reverse--)
 	putchar(reverse);
 	}
 	putchar(10);
-	return (0);
+return (0);
 }
