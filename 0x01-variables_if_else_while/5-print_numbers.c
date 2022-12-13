dbdd704fb8cabbc70b0168168dee1751c5e7ecb8
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-12 19:27:39 -0800

    Add a program that prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -A program that will print out num in base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int num = 48;
+
+	for (; num <= 57; num++)
+	{
+	putchar(num);
+	}
+	putchar(10);
+
+	return (0);
+}
