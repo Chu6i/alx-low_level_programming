c285e13095946f817d486cdcfd54c718956ed8ce
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-14 15:26:10 -0800

    fixed betty warning for exceeding characters

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
 * print_sign - Prints the sign of a number
 * @n: number input
-*Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
+*Return: 1, '+' if n > 0, 0, '0' if n = 0, -1, '-' if n < 0
 */
 
 int print_sign(int n)
