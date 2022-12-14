6cf381ad84a4377b5344045f120c1017daaf4802
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-13 22:31:21 -0800

    Add a script to print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the all the alphabets in lower case
+*/
+
+void print_alphabet(void)
+{
+	int alpha = 97;
+
+	for (; alpha <= 122; alpha++)
+	_putchar(alpha);
+	_putchar(10);
+}
