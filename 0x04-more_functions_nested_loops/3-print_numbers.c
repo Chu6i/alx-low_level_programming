c7023da060d9a29dc2b181919a5ee78cbe9aecce
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-15 07:47:05 -0800

    A function that prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - A function that printsthe numbers 0-9
+* Return : no return
+*/
+
+void print_numbers(void)
+{
+int num = 48;
+
+for (; num <= 57; num++)
+{
+_putchar(num);
+}
+_putchar(10);
+}
