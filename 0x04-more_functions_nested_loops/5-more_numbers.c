e5906cf493c9b915e941350f0c982ba08033e953
Okpala_Chibuike <okpalaemmanuel12@gmail.com>
2022-12-15 10:34:47 -0800

    Add a function that prints a digits 0-14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - A func that prints 10 times the number 1-14
+*/
+
+void more_numbers(void)
+{
+	int s;
+	int n;
+
+	for (s = 0; s <= 9; s++)
+	{
+
+	for (n = 0; n < 15; n++)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar(s);
+	_putchar(10);
+	}
+}
