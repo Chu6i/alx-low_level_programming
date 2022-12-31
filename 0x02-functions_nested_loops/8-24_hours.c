29a63dad933459f6eb90a477456286038dad1723
Chubi <okpalaemmanuel12@gmail.com>
2022-12-31 15:31:44 +0100

    Add  a function that prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ *jack_bauer - A func that prints every minute of the day
+ *
+ */
+
+void jack_bauer(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 23; m++)
+	{
+		for (n = m; n <= 59; n++)
+		{
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
