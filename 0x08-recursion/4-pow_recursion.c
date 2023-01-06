26af768570db089ca9a55324d84c1058c70af1e0
Chubi <okpalaemmanuel12@gmail.com>
2023-01-06 03:33:13 +0100

    A func that returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _pow_recursion - A func that print a no to the power of another no.
+ * @x: the inputed number
+ * @y: the inputed no. raised to the power
+ * Return: -1 if it is an error or value of x**y if sucess
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+}
