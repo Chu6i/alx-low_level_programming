60f68df00faf7949d83fca8c0c481ac2556d2d82
Chubi <okpalaemmanuel12@gmail.com>
2023-01-06 01:28:11 +0100

    A func that prints the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _strlen_recursion - A func that prints the length of a string
+ * @s: The inputed string
+ * Return: Total number of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s == 0)
+	{
+		return(0);
+	}
+	else
+	{
+		i = 1 + _strlen_recursion(s + 1);
+		return(i);
+	}
+}
