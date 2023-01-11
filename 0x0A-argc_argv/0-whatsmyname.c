1d9fa05e54c31b63a6e15e406134fe3f23df71e7
Chubi <okpalaemmanuel12@gmail.com>
2023-01-11 15:55:39 -0800

    Add a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main -Print the name of the file followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
