f29390a64e9de98725cac1d31f3bcfe21a551884
Chubi <okpalaemmanuel12@gmail.com>
2023-02-03 15:21:59 -0800

    Fixed bettu style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int max_bits = sizeof(unsigned long int) * 8;
+
 	if (index >= max_bits)
-		return -1;
-	return (n & (1UL << index)) != 0;
+		return (-1);
+	return ((n & (1UL << index)) != 0);
 }
 
